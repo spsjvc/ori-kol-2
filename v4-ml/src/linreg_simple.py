e3d7de31cdb56cacda6cdc6b0a9be87b536ce2e6
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2018-05-30 00:16:14 +0200

    Implement linear regression

--- v4-ml/src/linreg_simple.py
+++ v4-ml/src/linreg_simple.py
@@ -1,14 +1,15 @@
 from __future__ import print_function
 
-
+import math
+import numpy
 import random
 import matplotlib.pyplot as plt
 
-
 def linear_regression(x, y):
-    slope = 0.0  # nagib linije
-    intercept = 0.0  # tacka preseka na y-osi
     # TODO 1: implementirati linearnu regresiju
+
+    slope = cov(x, y) / var(x)
+    intercept = numpy.mean(y) - slope * numpy.mean(x)
 
     return slope, intercept
 
@@ -16,12 +17,22 @@ def linear_regression(x, y):
 def predict(x, slope, intercept):
     # TODO 2: implementirati racunanje y na osnovu x i parametara linije
 
-    return 0.0
+    return intercept + x * slope;
 
 
 def create_line(x, slope, intercept):
     y = [predict(xx, slope, intercept) for xx in x]
     return y
+
+
+def cov(x, y):
+    result = ((x[i] - numpy.mean(x)) * (y[i] - numpy.mean(y)) for i in range(len(x)))
+    return sum(list(result))
+
+
+def var(x):
+    result = (math.pow(x[i] - numpy.mean(x), 2) for i in range(len(x)))
+    return sum(list(result))
 
 
 if __name__ == '__main__':
